bf6c38e4067fb0f873b36cb2b08e712dc0e10220
Lucas Rafael <lucasrafaelalves@yahoo.com.br>
2023-07-02 03:08:06 -0300

    tela inicial

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const database = require('../db.js');
+const connection = require('../db.js');
 
 router.get('/', function(req, res, next) {
-    res.render('pages/movies',{ session : req.session} );
+
+    query = `
+        SELECT * FROM filme
+        `;
+        connection.query(query,(error,resultado) => {
+
+            if(error)throw error;
+        
+            if(!error){
+                console.log(resultado);
+                res.render('pages/movies',{ session : req.session, data: resultado});
+                
+            }
+        })
+        
+        
+        
+
+       
+
+
+
+    
 });
 
 
