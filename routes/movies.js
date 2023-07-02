9d3bce69f57c3c85dea566b8f5bdd68721aa76f2
Lucas Rafael <lucasrafaelalves@yahoo.com.br>
2023-07-02 15:35:39 -0300

    tela inicial funcional2

--- routes/movies.js
+++ routes/movies.js
@@ -2,19 +2,35 @@ const express = require('express');
 const router = express.Router();
 const database = require('../db.js');
 const connection = require('../db.js');
+const session = require('express-session');
 
 router.get('/', function(req, res, next) {
 
+    var cpf =  req.session.cpf;
     query = `
         SELECT * FROM filme
         `;
+    query2 = `select filme.id, filme.nome, filme.ano, filme.diretor, filme.notaimdb,
+    filme.classificacao, filme.sinopse from usuario inner join deseja_assistir on usuario.cpf = 
+    deseja_assistir.cpf_usuario inner join filme on deseja_assistir.id_filme =
+    filme.id where usuario.cpf = '${cpf}'`;
         connection.query(query,(error,resultado) => {
+            
 
             if(error)throw error;
         
             if(!error){
-                console.log(resultado);
-                res.render('pages/movies',{ session : req.session, data: resultado});
+                
+                connection.query(query2,(error2,resultado2) =>{
+                    if(error2)throw error2;
+
+                    if(!error2){
+                    console.log(resultado2);
+                
+                    res.render('pages/movies',{ session : req.session, data: resultado, data2: resultado2});
+                    }
+
+                })
                 
             }
         })
@@ -22,7 +38,6 @@ router.get('/', function(req, res, next) {
         
         
 
-       
 
 
 
@@ -30,4 +45,5 @@ router.get('/', function(req, res, next) {
 });
 
 
-module.exports = router;
+module.exports = router; 
+
