9296f19743baadabd160c521d77b4b82fcb8d09b
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2023-07-03 15:55:23 -0300

    Lista de desejos

--- routes/movies.js
+++ routes/movies.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const database = require('../db.js');
 const connection = require('../db.js');
-const session = require('express-session');
+// const session = require('express-session');
 
 router.get('/', function(req, res, next) {
 
@@ -39,14 +39,61 @@ router.get('/', function(req, res, next) {
                 
             }
         })
-        
-        
-        
-
-
-
 
     
+});
+
+
+router.post('/', function(req, res, next) {
+    if(req.body.item1 || req.body.item2) { // Adicionar ou remover da "Lista de desejos"
+        console.log(req.body);
+        console.log(req.session);
+
+        var item1 = req.body.item1;
+        var item2 = req.body.item2;
+
+        var cpf = req.session.cpf;
+
+        if(item1){
+            query = `INSERT INTO deseja_assistir (id_filme, cpf_usuario) VALUES(
+                ${item1}, "${cpf}"
+            );`;
+
+            database.query(query, function(error, data) {
+                if(!error) {
+                    res.redirect('/movies');
+                    return;
+                }
+                else {
+                    console.log(error);
+                    if(error.code === "ER_DUP_ENTRY") { // Já adicionou à lista de desejos
+                        return;
+                    }
+                    else {
+                        console.log(error);
+                        res.status(440).send("Erro no post do movies");
+                        return;
+                    }
+                }
+            });
+        }
+        else if(item2) {
+            query = `DELETE FROM deseja_assistir 
+                WHERE cpf_usuario = "${cpf}" AND id_filme = ${item2};`;
+
+            database.query(query, function(error, data) {
+                if(!error) {
+                    res.redirect('/movies');
+                    return;
+                }
+                else {
+                    console.log(error);
+                    res.status(440).send("Erro no post do movies");
+                    return;
+                }
+            });
+        }
+    }
 });
 
 
