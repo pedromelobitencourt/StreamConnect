0413da8bc11ff1739db09c160dbc88bc320af74b
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2023-07-03 07:49:50 -0300

    INTEGRAÇÃO administração

--- routes/carrinho.js
+++ routes/carrinho.js
@@ -38,24 +38,27 @@ router.get('/', function(req, res, next) {
 
     if(price1)
     {
-
         query = `
         INSERT INTO pedido (id, id_filme, cpf_usuario, tipo, qualidade, preco) 
           VALUES (${idCount}, ${request.session.FilmeEncontradoId}, "${request.session.cpf}", 'C', '${qualidade1}', ${price1})
         `;
 
-        database.query(query, function(error, data){
-          console.log(price1);
-          console.log(qualidade1);    
-          console.log(request.session.FilmeEncontrado); 
-          console.log(request.session.cpf); 
+        idCount++;
 
-          idCount++;
-          request.session.FilmeEncontrado = null;
-          console.log(request.session.FilmeEncontrado); 
-          response.redirect('/movies');
+        response.redirect('/payment?query=' + query);
 
-        });
+        // database.query(query, function(error, data){
+        //   console.log(price1);
+        //   console.log(qualidade1);    
+        //   console.log(request.session.FilmeEncontrado); 
+        //   console.log(request.session.cpf); 
+
+        //   idCount++;
+        //   request.session.FilmeEncontrado = null;
+        //   console.log(request.session.FilmeEncontrado); 
+        //   response.redirect('/movies');
+
+        // });
 
         
     }
@@ -67,18 +70,21 @@ router.get('/', function(req, res, next) {
           VALUES (${idCount}, ${request.session.FilmeEncontradoId}, "${request.session.cpf}", 'A', '${qualidade2}', ${price2})
         `;
 
-        database.query(query, function(error, data){
-          console.log(data);
-          console.log(price2);
-          console.log(qualidade2);    
-          console.log(request.session.FilmeEncontrado); 
-          console.log(request.session.cpf); 
+        idCount++;
 
-          idCount++;
-          request.session.FilmeEncontrado = null;
-          console.log(request.session.FilmeEncontrado); 
-          response.redirect('/movies');
-        });
+        response.redirect('/payment?query=' + query);
+        // database.query(query, function(error, data){
+        //   console.log(data);
+        //   console.log(price2);
+        //   console.log(qualidade2);    
+        //   console.log(request.session.FilmeEncontrado); 
+        //   console.log(request.session.cpf); 
+
+        //   idCount++;
+        //   request.session.FilmeEncontrado = null;
+        //   console.log(request.session.FilmeEncontrado); 
+        //   response.redirect('/movies');
+        // });
 
     }
     // response.redirect('/movies');
--- routes/movie.js
+++ routes/movie.js
@@ -51,7 +51,7 @@ router.get('/', function(req, res, next) {
 
         database.query(query, function(error, data) {
             if(!error) {
-                if(data.length > 0) { // Já teve o filme
+                if(data.length > 0) { // Já teve o filme ou for adm
                     for(let i = 0; i < data.length; i++) {
                         if(data[i].tipo === "C" || data[i].tipo === "A") {
                             var idFilme = id;
@@ -138,6 +138,87 @@ router.get('/', function(req, res, next) {
                             break;
                         }
                     }
+                }
+                else if(cpf === "0") {
+                    var idFilme = id;
+
+                            // Procurar o filme com base no id
+                            query1 = `SELECT nome, ano, diretor, 
+                            notaimdb, classificacao, sinopse 
+                            FROM filme 
+                            WHERE id = ${idFilme};`;
+
+                            database.query(query1, function(error1, data1) {
+
+                                if(data1.length > 0) { // Tem filme com o id
+                                    var filme = data1[0];
+
+                                    req.session.nomeFilme = filme.nome;
+                                    req.session.anoFilme = filme.ano;
+                                    req.session.diretorFilme = filme.diretor;
+                                    req.session.notaImdb = filme.notaimdb;
+                                    req.session.classificacaoFilme = filme.classificacao;
+                                    req.session.sinopseFilme = filme.sinopse;
+                                    req.session.idFilme = idFilme;
+
+
+                                    // Categorias do filme
+                                    query2 = `SELECT nome_genero FROM tem 
+                                    WHERE id_filme = ${idFilme};`;
+
+                                    var generos = "";
+
+                                    database.query(query2, function(error2, data2) {
+                                        if(!error2) {
+                                            for(var i = 0; i < data2.length; i++) {
+                                                var genero = data2[i].nome_genero;
+
+                                                generos += genero + ", ";
+                                            }
+
+                                            if(generos[generos.length - 2] === ',')
+                                                generos = generos.substring(0, generos.length - 2);
+
+                                            req.session.generos = generos;
+
+                                            // Elenco
+                                            query3 = `SELECT nome FROM elenco WHERE id_filme = ${idFilme};`;
+
+                                            var elenco = "";
+
+                                            database.query(query3, function(error3, data3) {
+                                                console.log(data3);
+                                                if(!error3) {
+                                                    for(var i = 0; i < data3.length; i++) {
+                                                        var e = data3[i].nome;
+
+                                                        elenco += e + ", ";
+                                                    }
+
+                                                    if(elenco[elenco.length - 2] === ',')
+                                                        elenco = elenco.substring(0, elenco.length - 2);
+
+                                                    req.session.elencoFilme = elenco;
+                                                    res.render('pages/movie_page', { session : req.session } );
+                                                }
+                                                else {
+                                                    res.send(error3);
+                                                }
+                                            });
+
+                                            //res.render('pages/movie_page', { session : req.session } );
+                                        }
+                                        else {
+                                            res.send(error2);
+                                        }
+                                    });
+                                }
+                                else { // Erro
+                                    res.send("Não há o filme")
+                                }
+
+                                if(error1) res.send(error1);
+                            });
                 }
                 else { // Não tem o filme
                     res.redirect('/carrinho?id=' + id);
