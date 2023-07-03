ad755de1d6416077f58b7792a5c56d2fbacb88bd
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2023-07-03 16:54:18 -0300

    Principal

--- routes/movies.js
+++ routes/movies.js
@@ -3,105 +3,7 @@ const router = express.Router();
 const database = require('../db.js');
 const connection = require('../db.js');
 // const session = require('express-session');
-
-router.post('/', function(req, res, next) {
-    var busca =  req.body.busca;
-    console.log(busca)
-    var cpf =  req.session.cpf;
-    if(busca==''){
     
-    query = `
-        SELECT * FROM filme
-        `;
-    query2 = `select filme.id, filme.nome, filme.ano, filme.diretor, filme.notaimdb,
-    filme.classificacao, filme.sinopse from usuario inner join deseja_assistir on usuario.cpf = 
-    deseja_assistir.cpf_usuario inner join filme on deseja_assistir.id_filme =
-    filme.id where usuario.cpf = '${cpf}'`;
-    
-    query3 = `select distinct filme.nome, filme.ano, filme.diretor, filme.notaimdb,
-    filme.classificacao, filme.sinopse from
-    filme inner join pedido on filme.id =  pedido.id_filme inner join
-    usuario on pedido.cpf_usuario = usuario.cpf where usuario.cpf = '${cpf}'`;
-        connection.query(query,(error,resultado) => {
-            
-
-            if(error)throw error;
-        
-            if(!error){
-                
-                connection.query(query2,(error2,resultado2) =>{
-                    if(error2)throw error2;
-
-                    if(!error2){
-                        connection.query(query3,(error3,resultado3) =>{
-
-                            if(error3)throw error3;
-
-                            if(!error3){
-
-                                res.render('pages/movies',{ session : req.session, data: resultado, data2: resultado2, data3: resultado3});
-
-                            }
-                            
-                        })   
-                    
-                
-                    }
-
-                })
-                
-            }
-        })}
-        else{
-    
-    query = `
-        SELECT * FROM filme where filme.nome like '%${busca}%'
-        `;
-    query2 = `select filme.id, filme.nome, filme.ano, filme.diretor, filme.notaimdb,
-    filme.classificacao, filme.sinopse from usuario inner join deseja_assistir on usuario.cpf = 
-    deseja_assistir.cpf_usuario inner join filme on deseja_assistir.id_filme =
-    filme.id where usuario.cpf = '${cpf}' and filme.nome like '%${busca}%'`;
-
-    query3 = `select distinct filme.nome, filme.ano, filme.diretor, filme.notaimdb,
-    filme.classificacao, filme.sinopse from
-    filme inner join pedido on filme.id =  pedido.id_filme inner join
-    usuario on pedido.cpf_usuario = usuario.cpf where usuario.cpf = '${cpf}' 
-    and filme.nome like '%${busca}%'`;
-        connection.query(query,(error,resultado) => {
-            
-
-            if(error)throw error;
-        
-            if(!error){
-                
-                connection.query(query2,(error2,resultado2) =>{
-                    if(error2)throw error2;
-
-                    if(!error2){
-                        connection.query(query3,(error3,resultado3) =>{
-
-                            if(error3)throw error3;
-
-                            if(!error3){
-
-                                res.render('pages/movies',{ session : req.session, data: resultado, data2: resultado2, data3: resultado3});
-
-                            }
-                            
-                        })   
-                    }
-
-                })
-                
-            }
-        })
-
-        }
-        
-        
-
-
-})
 
 
 router.get('/', function(req, res, next) {
@@ -111,9 +13,6 @@ router.get('/', function(req, res, next) {
         return;
     }
 
-    
-
-    
     var cpf =  req.session.cpf;
     query = `
         SELECT * FROM filme
@@ -124,7 +23,7 @@ router.get('/', function(req, res, next) {
     filme.id where usuario.cpf = '${cpf}'`;
 
     query3 = `select distinct filme.nome, filme.ano, filme.diretor, filme.notaimdb,
-    filme.classificacao, filme.sinopse from
+    filme.classificacao, filme.sinopse, filme.id from
     filme inner join pedido on filme.id =  pedido.id_filme inner join
     usuario on pedido.cpf_usuario = usuario.cpf where usuario.cpf = '${cpf}'`;
         connection.query(query,(error,resultado) => {
@@ -163,6 +62,92 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/', function(req, res, next) {
+    if(!req.session.username) { // Não está logado
+        res.redirect('signin');
+        return;
+    }
+
+    var busca =  req.body.busca;
+    console.log(busca)
+    var cpf =  req.session.cpf;
+    if(busca==''){
+        
+        query = `
+            SELECT * FROM filme
+            `;
+
+        query2 = `select filme.id, filme.nome, filme.ano, filme.diretor, filme.notaimdb,
+        filme.classificacao, filme.sinopse from usuario inner join deseja_assistir on usuario.cpf = 
+        deseja_assistir.cpf_usuario inner join filme on deseja_assistir.id_filme =
+        filme.id where usuario.cpf = '${cpf}'`;
+        
+        query3 = `select distinct filme.nome, filme.ano, filme.diretor, filme.notaimdb,
+        filme.classificacao, filme.sinopse, filme.id from
+        filme inner join pedido on filme.id =  pedido.id_filme inner join
+        usuario on pedido.cpf_usuario = usuario.cpf where usuario.cpf = '${cpf}'`;
+
+        connection.query(query,(error,resultado) => {
+            if(error)throw error;
+            
+            if(!error){
+                connection.query(query2,(error2,resultado2) =>{
+                if(error2)throw error2;
+
+                    if(!error2){
+                        connection.query(query3,(error3,resultado3) =>{
+                            if(error3)throw error3;
+
+                            if(!error3){
+                                res.render('pages/movies',{ session : req.session, data: resultado, data2: resultado2, data3: resultado3});
+                                return;
+                            }    
+                        });   
+                    }
+
+                });
+                    
+            }
+        });
+    }
+    else{
+        query = `
+            SELECT * FROM filme where filme.nome like '%${busca}%'
+             `;
+
+        query2 = `select filme.id, filme.nome, filme.ano, filme.diretor, filme.notaimdb,
+        filme.classificacao, filme.sinopse from usuario inner join deseja_assistir on usuario.cpf = 
+        deseja_assistir.cpf_usuario inner join filme on deseja_assistir.id_filme =
+        filme.id where usuario.cpf = '${cpf}' and filme.nome like '%${busca}%'`;
+
+        query3 = `select distinct filme.nome, filme.ano, filme.diretor, filme.notaimdb,
+        filme.classificacao, filme.sinopse, filme.id from
+        filme inner join pedido on filme.id =  pedido.id_filme inner join
+        usuario on pedido.cpf_usuario = usuario.cpf where usuario.cpf = '${cpf}' 
+        and filme.nome like '%${busca}%'`;
+
+        connection.query(query,(error,resultado) => {
+            if(error)throw error;
+                    
+            if(!error){
+                connection.query(query2,(error2,resultado2) =>{
+                    if(error2)throw error2;
+
+                    if(!error2){
+                        connection.query(query3,(error3,resultado3) =>{
+                        
+                        if(error3)throw error3;
+
+                        if(!error3){
+                            console.log("ohoihxoih: ", resultado3);
+                            res.render('pages/movies',{ session : req.session, data: resultado, data2: resultado2, data3: resultado3});
+                            return;
+                        }
+                        });   
+                    }
+                });   
+            }
+        });
+    }
     if(req.body.item1 || req.body.item2) { // Adicionar ou remover da "Lista de desejos"
         console.log(req.body);
         console.log(req.session);
